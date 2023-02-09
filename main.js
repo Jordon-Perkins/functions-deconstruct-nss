9b4af990231d588161bf9852c8c7ad3e9cb431e8
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-09 09:18:32 -0600

    only commit

---
+++ main.js
@@ -0,0 +1,49 @@
+// find inspiration
+// find fabric
+// cut our pattern and fabric
+// sew and press seams
+// final touches and hand sewing
+// fitting with model
+// photo shoot for longevity 
+
+
+const sewPress = () => {
+	console.log('sew and press each seam ')
+}
+
+const inspiration = () => {
+	console.log('watch a movie and search Pinterest')
+}
+
+const photoShoot = () => {
+	console.log('decide between studio or location, date and time')
+}
+
+const fabric = () => {
+	console.log('check my stash, shop online or in store')
+}
+
+const cut = () => {
+    console.log('cut the pattern out if needed and then start in on the fabric for all layer of the garment or garments')
+}
+
+const finalTouches = () => {
+    console.log('trims and other notions as well as closures and hem that need to be don eby hand sewing')
+}
+
+const fitting = () => {
+	console.log('making sure the dang thing fits!!!!!!')
+}
+
+
+inspiration()
+fabric()
+cut()
+sewPress()
+finalTouches()
+fitting()
+photoShoot() 
+
+//each functon starts with a variable based on the step.
+//the body takes a log to the console of the details of said step
+//each function is called in the order with which it must be exacuted 
